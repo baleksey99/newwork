1a9f5105b2c818a6668abce970cbdde30dbe0049
Алексей <abe26@yandex.ru>
2025-11-14 19:27:38 +0300

    корректировка

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from src.classes import Product
+from src.classes import Category
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
@@ -19,8 +21,8 @@ if __name__ == "__main__":
     print(product3.price)
     print(product3.quantity)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+    category1 = Category("Смартфоны", "Смартфоны, как средство не только коммуникации, "
+                         "но и получения дополнительных функций для удобства жизни",
                          [product1, product2, product3])
 
     print(category1.name == "Смартфоны")
@@ -30,9 +32,8 @@ if __name__ == "__main__":
     print(category1.product_count)
 
     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+    category2 = Category("Телевизоры", "Современный телевизор, который позволяет наслаждаться просмотром, "
+                         " станет вашим другом и помощником", [product4])
 
     print(category2.name)
     print(category2.description)
--- src/classes.py
+++ src/classes.py
@@ -8,8 +8,8 @@ class Product:
 
 class Category:
     # Атрибуты класса
-    total_categories = 0
-    total_products = 0
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name: str, description: str, products: list[Product] = None):
         self.name = name
@@ -17,5 +17,5 @@ class Category:
         self.products = products if products is not None else []
 
         # Автоматическое увеличение счётчиков при создании объекта
-        Category.total_categories += 1
-        Category.total_products += len(self.products)
+        Category.category_count += 1
+        Category.product_count += len(self.products)
